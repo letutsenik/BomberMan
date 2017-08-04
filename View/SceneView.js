6a51a7798e2f17a791af3135f3e86f089bafaf63
dmitri_lipski <dzmitri.lipski@gmail.com>
2017-08-04 16:16:10 +0300

    Updated SceneViev to ES6

--- View/SceneView.js
+++ View/SceneView.js
@@ -1,50 +1,50 @@
 'use strict';
-function TSceneView() {
-    var SceneModel = null;
-    var Scene = null;
+class TSceneView {
+    constructor() {
+        this._SceneModel = null;
+        this._Scene = null;
 
-    var BrickFiles = { 1:'SolidBlock.png',
-        2:'ExplodableBlock.png' };
-    
-    this.Init = function(Model,Elem) {
+        this._BrickFiles = {
+            1:'SolidBlock.png',
+            2:'ExplodableBlock.png',
+        };
+    }
 
-        SceneModel = Model;
-        Scene = Elem;
+    Init(Model,Elem) {
+        this._SceneModel = Model;
+        this._Scene = Elem;
     };
 
-    this.Update = function() {
-
+    Update() {
         // вычисляем размеры картинки
-        var BlockImgWidth = Scene.offsetWidth / SceneModel.HorzBlocks ;
-        var BlockImgHeight = Scene.offsetHeight / SceneModel.VertBlocks;
-        //console.log(BlockImgWidth);
-        //console.log(BlockImgHeight);
+        const BlockImgWidth = this._Scene.offsetWidth / this._SceneModel.HorzBlocks ;
+        const BlockImgHeight = this._Scene.offsetHeight / this._SceneModel.VertBlocks;
 
         // надо создать картинки кирпичей там где они нужны
         // а их ещё нет
         // и удалить там где они не нужны но были созданы
-        for ( var R = 0; R < SceneModel.VertBlocks; R++ )
-            for ( var C = 0; C < SceneModel.HorzBlocks; C++ )
+        for ( let R = 0; R < this._SceneModel.VertBlocks; R++ )
+            for ( let C = 0; C < this._SceneModel.HorzBlocks; C++ )
             {
-                var CellH = SceneModel.BlocksA[R][C];
-                if ( CellH.kind && !CellH.elem && CellH.kind!=4)
+                let CellH = this._SceneModel.BlocksA[R][C];
+                if ( CellH.kind && !CellH.elem && CellH.kind !== 4)
                 { // картинка кирпича нужна - создаём
                     CellH.elem = document.createElement('IMG');
                     CellH.elem.classList.add('block');
                     CellH.elem.style.width = BlockImgWidth + 'px';
                     CellH.elem.style.height = BlockImgHeight + 'px';
 
-                    var CoordsH = SceneModel.BlockToScreen(R,C);
-                    CellH.elem.style.left = CoordsH.left * (BlockImgWidth/SceneModel.BlockWidth) + 'px';
-                    CellH.elem.style.top = CoordsH.top * (BlockImgHeight/SceneModel.BlockHeight)+ 'px';
+                    let CoordsH = this._SceneModel.BlockToScreen(R,C);
+                    CellH.elem.style.left = CoordsH.left * (BlockImgWidth/this._SceneModel.BlockWidth) + 'px';
+                    CellH.elem.style.top = CoordsH.top * (BlockImgHeight/this._SceneModel.BlockHeight)+ 'px';
 
                     CellH.elem.src = 'images/'
-                        + BrickFiles[CellH.kind];
-                    Scene.appendChild(CellH.elem);
+                        + this._BrickFiles[CellH.kind];
+                    this._Scene.appendChild(CellH.elem);
                 }
                 if ( (!CellH.kind) && CellH.elem )
                 { // картинка кирпича не нужна - удаляем
-                    Scene.removeChild(CellH.elem);
+                    this._Scene.removeChild(CellH.elem);
                     CellH.elem = null;
                 }
             }
